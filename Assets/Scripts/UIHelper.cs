667955a3d6988b5b85ab2f76a8bf42141dd16e4e
CohiahtdeCoca <121209618+CohiahtdeCoca@users.noreply.github.com>
2024-08-26 17:40:50 +0700

    a

--- Assets/Scripts/UIHelper.cs
+++ Assets/Scripts/UIHelper.cs
@@ -1,25 +1,61 @@
-using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public static class UIHelper
 {
-    public static void AddButtonListener(GameObject parent, string buttonName, Action action)
+    public static void AddButtonListener(GameObject parent, string buttonName, UnityEngine.Events.UnityAction action)
     {
         Transform buttonTransform = parent.transform.Find(buttonName);
         if (buttonTransform != null)
         {
-            GameObject buttonObject = buttonTransform.gameObject;
-            if (buttonObject.GetComponent<Button>() == null)
-            {
-                buttonObject.AddComponent<Button>();
-            }
-            Button button = buttonObject.GetComponent<Button>();
-            button.onClick.AddListener(() => action());
+            Button button = buttonTransform.GetComponent<Button>() ?? buttonTransform.gameObject.AddComponent<Button>();
+            button.onClick.AddListener(action);
         }
         else
         {
-            Debug.Log($"{buttonName} not found");
+            Debug.LogWarning("Button " + buttonName + " not found in " + parent.name);
+        }
+    }
+
+    public static void SetButtonInteractable(GameObject parent, string buttonName, bool interactable)
+    {
+        Transform buttonTransform = parent.transform.Find(buttonName);
+        if (buttonTransform != null)
+        {
+            Button button = buttonTransform.GetComponent<Button>();
+            if (button != null)
+            {
+                button.interactable = interactable;
+            }
+            else
+            {
+                Debug.LogWarning("Button component not found on " + buttonName);
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Button " + buttonName + " not found in " + parent.name);
+        }
+    }
+
+    public static void SetButtonSprite(GameObject parent, string buttonName, Sprite sprite)
+    {
+        Transform buttonTransform = parent.transform.Find(buttonName);
+        if (buttonTransform != null)
+        {
+            Image buttonImage = buttonTransform.GetComponent<Image>();
+            if (buttonImage != null)
+            {
+                buttonImage.sprite = sprite;
+            }
+            else
+            {
+                Debug.LogWarning("Image component not found on " + buttonName);
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Button " + buttonName + " not found in " + parent.name);
         }
     }
 }
